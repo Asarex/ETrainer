b8d643324830fd42fa73d90ab1639ccde4addcd0
Alexander Gladkevich <Gladkevich.alexander@gmail.com>
2020-10-13 01:36:10 +0300

    Добавил регистрацию пользователей

---
+++ ETrainerWeb/Models/DbContexts/AppIdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETrainerWeb.Models.DbContexts
+{
+	public class AppIdentityDbContext : IdentityDbContext<User>
+	{
+		public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+			: base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
--- ETrainerWeb/Startup.cs
+++ ETrainerWeb/Startup.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ETrainerWeb.Models;
+using ETrainerWeb.Models.DbContexts;
 using ETrainerWeb.Models.Repositories;
 using ETrainerWeb.Models.Repositories.ExercisesRepositories;
 using ETrainerWeb.Models.Repositories.MusclesRepositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,12 +20,20 @@ namespace ETrainerWeb
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
 			services.AddRazorPages();
+			services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+			services.AddDbContext<AppIdentityDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
 #if DEBUG
 			services.AddScoped<IMusclesRepository, MusclesRepositoryFake>();
 			services.AddScoped<IExercisesRepository, ExercisesRepositoryFake>();
@@ -29,7 +42,6 @@ namespace ETrainerWeb
 #endif
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -38,6 +50,9 @@ namespace ETrainerWeb
 			}
 
 			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
