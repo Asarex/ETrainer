bdedad84845612370b24999cba0d6734d415f1e4
Alexander Gladkevich <Gladkevich.alexander@gmail.com>
2020-10-21 23:42:31 +0300

    Добавил панель администратора

---
+++ ETrainerWeb/Models/IdentityDataInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ETrainerWeb.Models
+{
+	public class IdentityDataInitializer
+	{
+		public static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+		{
+			var adminExist = await roleManager.RoleExistsAsync("Admin");
+			if (!adminExist)
+			{
+				var role = new IdentityRole("Admin");
+				await roleManager.CreateAsync(role);
+			}
+
+			var userExist = await roleManager.RoleExistsAsync("User");
+			if (!userExist)
+			{
+				var role = new IdentityRole("User");
+				await roleManager.CreateAsync(role);
+			}
+		}
+
+		public static async Task SeedUsers(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			var admin = await userManager.FindByNameAsync("Admin");
+			if (admin is null)
+			{
+				admin = new User { UserName = "Admin" };
+				var res = await userManager.CreateAsync(admin, "notAdmin1@");
+				if (res.Succeeded)
+				{
+					await userManager.AddToRoleAsync(admin, "Admin");
+				}
+			}
+		}
+	}
+}
--- ETrainerWeb/Program.cs
+++ ETrainerWeb/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ETrainerWeb.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,10 +14,31 @@ namespace ETrainerWeb
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
-		}
+			var host = CreateHostBuilder(args).Build();
+
+			using (var scope = host.Services.CreateScope())
+			{
+				var serviceProvider = scope.ServiceProvider;
+				try
+				{
+					var userManager = serviceProvider.
+						GetRequiredService<UserManager<User>>();
+
+					var roleManager = serviceProvider.
+						GetRequiredService<RoleManager<IdentityRole>>();
+
+					await IdentityDataInitializer.SeedRoles(roleManager);
+					await IdentityDataInitializer.SeedUsers(userManager, roleManager);
+				}
+				catch
+				{
+					// ignored
+				}
+			}
+			host.Run();
+        }
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
